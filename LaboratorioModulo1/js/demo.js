1e5e10b81882375f00d1b22a21cb7a1c9f4e034e
CNavas96 <c.navas.1996@gmail.com>
2022-06-22 19:57:47 +0200

    LaboratorioMODULO1

---
+++ LaboratorioModulo1/js/demo.js
@@ -0,0 +1,5 @@
+document.getElementById("name") .value = "Mi nombre";
+
+document.getElementById("lastname") .value = "Mi apellido";
+
+document.getElementById("avatar") .src = "imagenes/avatar.png"
