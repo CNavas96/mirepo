cf3a1aa3068f7cffd5bfb4da0edbc7e19d433fa1
CNavas96 <107400260+CNavas96@users.noreply.github.com>
2022-06-22 20:00:05 +0200

    Delete LaboratorioModulo1 directory

--- LaboratorioModulo1/js/demo.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("name") .value = "Mi nombre";
-
-document.getElementById("lastname") .value = "Mi apellido";
-
-document.getElementById("avatar") .src = "imagenes/avatar.png"
