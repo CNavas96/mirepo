698b2c4e94509d99acccac53cf0ce817edbe310f
CNavas96 <c.navas.1996@gmail.com>
2024-01-26 16:21:45 +0100

    Actualizando Laboratorio 2

--- Laboratorio2/src/main.ts
+++ Laboratorio2/src/main.ts
@@ -3,4 +3,4 @@ import "./style.css";
 console.log("Hello Typescript!");
 
 
-console.log((120 - 18) / 7 + " " + "Esto es lo que pagaria cada comensal")
+console.log((120 - 18) / 6 + " " + "Esto es lo que pagaria cada comensal")
