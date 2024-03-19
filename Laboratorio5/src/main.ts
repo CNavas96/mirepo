b6a42d1147436be9eeee719293ecac07d166fccf
CNavas96 <c.navas.1996@gmail.com>
2024-03-19 19:49:17 +0100

    arreglo de la carta y apartado opcional añadido

--- Laboratorio5/src/main.ts
+++ Laboratorio5/src/main.ts
@@ -1,6 +1,8 @@
 //1. Mostrar puntuación
 
-let score = 0;
+let score: number = 0;
+let cartaAdicional: number = 0;
+
 const mostrarPuntuacion = () : void => {
     const pantallaPuntuacion = document.getElementById("puntuacion");
 
@@ -20,16 +22,10 @@ mostrarPuntuacion();
 //2. Pedir carta   
 
 const dameCarta = () : number => {
-    let cartaAleatoria : number = Math.floor(Math.random() * 11);
-    if(cartaAleatoria === 0){
-        cartaAleatoria +=1
-    } else if(cartaAleatoria > 7){
+    let cartaAleatoria : number = Math.ceil(Math.random() * 10);
+    if(cartaAleatoria > 7){
         cartaAleatoria  += 2;
-    } else if (cartaAleatoria > 8) {
-        cartaAleatoria +=2;
-    } else if (cartaAleatoria > 9) {
-        cartaAleatoria +=2;
-    }
+    } 
     return cartaAleatoria;
 };
 
@@ -53,11 +49,11 @@ const cogerImagenCarta = (carta: number) : string => {
             break;
         case 7: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
             break;
-        case 8: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+        case 10: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
             break;
-        case 9: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+        case 11: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
             break;
-        case 10: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+        case 12: imagenCarta = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
             break;  
     }
     return imagenCarta;
@@ -73,7 +69,7 @@ function handlePedirCarta() : void {
             const cartaSrc = cogerImagenCarta(cartaObtenida);
             cartaRecibida.src = cartaSrc;
 
-            if(cartaObtenida > 9 && cartaObtenida <13){
+            if(cartaObtenida > 9 && cartaObtenida < 13){
                 cartaObtenida = 0.5;
             }
 
@@ -89,16 +85,31 @@ function handlePlantarse () : void {
         if(pantallaPuntuacion !== null && pantallaPuntuacion !== undefined && pantallaPuntuacion instanceof HTMLHeadingElement) {
             pantallaPuntuacion.textContent = score.toString();
 
-            if(score < 4){
+            if(score <= 4 || score < 5){
                 pantallaPuntuacion.textContent = "Has sido muy conservador";
-            } else if(score === 5){
+            } else if(score === 5 || score < 6){
                 pantallaPuntuacion.textContent ="No huele un poco raro aqui?";
             } else if(score >= 6 && score <= 7){
                 pantallaPuntuacion.textContent ="Por porquito...";
             } else if(score === 7.5){
                 pantallaPuntuacion.textContent ="LETS FUCKING GO!!! LO HAS LOGRADO"
             }
-        }    
+        } 
+        
+        if(score < 7.5){
+            cartaAdicional = dameCarta();
+
+            const imagenCartaAdicional = document.getElementById("carta-adicional");
+            const botonCartaAdicional = document.getElementById("boton-mostrar-carta-adicional");
+                if (imagenCartaAdicional !== null && imagenCartaAdicional !== undefined && imagenCartaAdicional instanceof HTMLImageElement &&
+                botonCartaAdicional !== null && botonCartaAdicional !== undefined && botonCartaAdicional instanceof HTMLButtonElement) {
+                imagenCartaAdicional.style.display = "inline";
+                botonCartaAdicional.style.display = "inline";
+                }
+        }
+        
+    
+       
 }
 
 function handleNuevaPartida () : void {
@@ -112,10 +123,24 @@ function handleNuevaPartida () : void {
                 botonPideCarta.disabled = false;
                 botonPlantarse.disabled = false;
         }
-        console.log("HAS EMPEZADO UNA NUEVA PARTIDA");
+
+    const imagenCartaAdicional = document.getElementById("carta-adicional");
+    const botonCartaAdicional = document.getElementById("boton-mostrar-carta-adicional");
+    if (imagenCartaAdicional !== null && imagenCartaAdicional !== undefined && imagenCartaAdicional instanceof HTMLImageElement &&
+        botonCartaAdicional !== null && botonCartaAdicional !== undefined && botonCartaAdicional instanceof HTMLButtonElement) {
+        imagenCartaAdicional.style.display = "none";
+        botonCartaAdicional.style.display = "none";
+        imagenCartaAdicional.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+    }
 }
 
-
+function handleCartaAdicional () : void {
+    const imagenCartaAdicional = document.getElementById("carta-adicional");
+    if(imagenCartaAdicional !== null && imagenCartaAdicional !== undefined && imagenCartaAdicional instanceof HTMLImageElement){
+        const cartaAdicionalSrc = cogerImagenCarta(cartaAdicional);
+        imagenCartaAdicional.src = cartaAdicionalSrc;
+    }
+}
 
 // BOTON PEDIR CARTA
 const botonDameCarta = document.getElementById("boton-dameCarta");
@@ -125,38 +150,46 @@ const botonDameCarta = document.getElementById("boton-dameCarta");
 
 //BOTON PLANTARSE
 const botonPlantarse = document.getElementById("boton-plantarse");
-if(botonPlantarse !== null && botonPlantarse !== undefined && botonPlantarse instanceof HTMLButtonElement) {
-    botonPlantarse.addEventListener("click", handlePlantarse);   
-}
+    if(botonPlantarse !== null && botonPlantarse !== undefined && botonPlantarse instanceof HTMLButtonElement) {
+        botonPlantarse.addEventListener("click", handlePlantarse);   
+    }
 
 //BOTON NUEVA PARTIDA
 const botonNuevaPartida = document.getElementById("nueva-partida");
     if(botonNuevaPartida !== null && botonNuevaPartida !== undefined && botonNuevaPartida instanceof HTMLButtonElement) {
         botonNuevaPartida.addEventListener("click", handleNuevaPartida);   
     }
+
+//BOTON CARTA ADICIONAL
+const botonCartaAdicional = document.getElementById("boton-mostrar-carta-adicional");
+    if(botonCartaAdicional !== null && botonCartaAdicional !== undefined && botonCartaAdicional instanceof HTMLButtonElement){
+        botonCartaAdicional.addEventListener("click", handleCartaAdicional);
+        botonCartaAdicional.style.display = "none";
+    }
+
 // DESACTIVAR BOTONES
 const desactivarBotones = () : void => {
     const botonDameCarta = document.getElementById("boton-dameCarta");
-    const botonPlantarse = document.getElementById("boton-plantarse")
+    const botonPlantarse = document.getElementById("boton-plantarse");
 
     if (botonDameCarta !== null && botonDameCarta !== undefined && botonDameCarta instanceof HTMLButtonElement &&
         botonPlantarse !== null && botonPlantarse !== undefined && botonPlantarse instanceof HTMLButtonElement) {
 
-            if(score < 4){
-                botonDameCarta.disabled = true;
-                botonPlantarse.disabled = true;
-            } else if(score === 5){
-                botonDameCarta.disabled = true;
-                botonPlantarse.disabled = true;
-            } else if(score >= 6 && score <= 7){
-                botonDameCarta.disabled = true;
-                botonPlantarse.disabled = true;
-            } else if(score === 7.5){
-                botonDameCarta.disabled = true;
-                botonPlantarse.disabled = true;
-            } else if(score > 7.5){
-                botonDameCarta.disabled = true;
-                botonPlantarse.disabled = true; 
+        if(score <= 4){
+            botonDameCarta.disabled = true;
+            botonPlantarse.disabled = true;
+        } else if(score === 5){
+            botonDameCarta.disabled = true;
+            botonPlantarse.disabled = true;
+        } else if(score >= 6 && score <= 7){
+            botonDameCarta.disabled = true;
+            botonPlantarse.disabled = true;
+        } else if(score === 7.5){
+            botonDameCarta.disabled = true;
+            botonPlantarse.disabled = true;
+        } else if(score > 7.5){
+            botonDameCarta.disabled = true;
+            botonPlantarse.disabled = true; 
         } 
         
     }
