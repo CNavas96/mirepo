648cf497dcbb997eea933b5b7e46595554b821ba
CNavas96 <107400260+CNavas96@users.noreply.github.com>
2024-05-01 17:19:26 +0200

    Delete Prueba JS directory

--- Prueba JS/src/validanif.helper.js
+++
@@ -1,5 +0,0 @@
-const __CUANTAS_LETRAS = 23;
-const __LETRAS = "TRWAGMYFPDXBNJZSQVHLCKE";
-
-export const calculaIndiceLetra = (numero) => numero % __CUANTAS_LETRAS;
-export const obtenLetra = (indice) => __LETRAS[indice];
