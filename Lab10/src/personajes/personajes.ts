c0bc51a77d01b9b5ead4898bb8dac0f73c6f716d
CNavas96 <c.navas.1996@gmail.com>
2024-05-10 18:03:42 +0200

    Laboratorio 10 Asincronia terminado

--- Lab10/src/personajes/personajes.ts
+++ Lab10/src/personajes/personajes.ts
@@ -10,11 +10,16 @@ const crearParrafo = (texto: string): HTMLParagraphElement => {
     return parrafo;
 };
 
-const crearImagen = (imagen: string): HTMLImageElement => {
+const crearImagen = (imagen: string, nombrePersonaje: string): HTMLImageElement => {
     const imagenPersonaje = document.createElement("img");
+
     imagenPersonaje.src = imagen;
+    imagenPersonaje.alt = nombrePersonaje;
     return imagenPersonaje;
 };
+
+
+//concatenar la url `http://localhost:3000/${personaje.imagen}` 
 
 const crearContenedorPersonaje =  (personaje: Personajes): HTMLDivElement => {
 
@@ -22,20 +27,20 @@ const crearContenedorPersonaje =  (personaje: Personajes): HTMLDivElement => {
     divPersonajes.classList.add("personaje-contenedor");
 
     if(divPersonajes && divPersonajes instanceof HTMLDivElement){
-        const imagen = crearImagen(personaje.imagen);
+        
+        const imagen = crearImagen(`http://localhost:3000/${personaje.imagen}`, personaje.nombre);
         divPersonajes.appendChild(imagen);
 
-        const nombre = crearParrafo(personaje.nombre);
+        const nombre = crearParrafo(`Nombre: ${personaje.nombre}`);
         divPersonajes.appendChild(nombre);
 
-        const especialidad = crearParrafo(personaje.especialidad);
+        const especialidad = crearParrafo(`Especialidad: ${personaje.especialidad}`);
         divPersonajes.appendChild(especialidad);
 
-        personaje.habilidades.forEach((habilidad) => {
-            const textoHabilidades = crearParrafo(habilidad);
-            divPersonajes.appendChild(textoHabilidades);
-        });
-    
+        const habilidades = document.createElement('p');
+        habilidades.textContent = `Habilidades: ${personaje.habilidades}`;
+        divPersonajes.appendChild(habilidades);
+
         return divPersonajes;
 
     } else{
@@ -45,11 +50,13 @@ const crearContenedorPersonaje =  (personaje: Personajes): HTMLDivElement => {
 
 const pintarPersonajes = async () => {
     const personajes = await obtenerPersonajes();
-    const listado = document.querySelector("listado-personajes");
+    console.log(personajes);
+    const listado = document.querySelector("#listado-personajes");
     
     if(listado && listado instanceof HTMLDivElement){
         personajes.forEach((personaje) => {
             const contenedorPersonaje = crearContenedorPersonaje(personaje);
+            console.log(contenedorPersonaje);
             listado.appendChild(contenedorPersonaje);
         })
     } else {
@@ -57,6 +64,66 @@ const pintarPersonajes = async () => {
     }
 };
 
-//ERROR EN LA CONSOLA PREGUNTAR
+
+const buscarPersonaje = (nombre: string, personajes: Personajes[]): Personajes[] => {
+    return personajes.filter(personaje =>
+      personaje.nombre.toLowerCase().includes(nombre.toLowerCase())
+    );
+  };
+
+
+const manejadorBusqueda = async (event: Event): Promise<void> =>{
+    event.preventDefault();
+
+    const inputBuscar = document.querySelector("#input-buscar");
+    if(inputBuscar && inputBuscar instanceof HTMLInputElement){
+        const nombre = inputBuscar.value;
+        const personajes = await obtenerPersonajes();
+    
+        limpiarLista();
+        resultadoBusqueda(nombre, personajes);
+    } else {
+        throw new Error("El nombre no existe");
+    }
+};
+
+
+const resultadoBusqueda = (nombre: string, personajes: Personajes[]): void => {
+    const busqueda = buscarPersonaje(nombre, personajes);
+    if(busqueda.length > 0) {
+
+        busqueda.forEach((personaje) => {
+        const personajebuscado = crearContenedorPersonaje(personaje);
+
+        const listado = document.querySelector("#listado-personajes");
+            if(listado && listado instanceof HTMLDivElement){
+                listado.appendChild(personajebuscado);
+            }
+        })
+    } else {
+        alert("No existe ese personaje o has escrito mal el nombre");
+    }
+};       
+
+
+
+const limpiarLista = () => {
+    const listado = document.querySelector("#listado-personajes");
+    if(listado && listado instanceof HTMLDivElement){
+        listado.innerHTML ="";
+    }
+};
+
+
+const botonBuscar = document.querySelector("#boton-buscar");
+
+if (botonBuscar && botonBuscar instanceof HTMLButtonElement) {
+    botonBuscar.addEventListener("click", manejadorBusqueda); 
+  } else {
+    throw new Error("boton no funciona");
+  }
+
+  
+
 
 document.addEventListener("DOMContentLoaded", pintarPersonajes);
